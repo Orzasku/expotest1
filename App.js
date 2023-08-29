8883e44359a543b4bbecf0eaebe758dcc238f5c7
Ozku <nikojuhani90@gmail.com>
2023-08-29 18:21:30 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [euros, setEuros] = useState("")
+  const [krona, setKrona] = useState(0)
+  const calculate = () => {
+    const result = euros.replace(',','.') * 11.85
+    setKrona(result)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Eurot</Text>
+      <TextInput placeholder='Laita euromäärä'
+      keyboardType='decimal-pad'
+      value={euros}
+      onChangeText={text => setEuros(text)}
+      style={styles.field}/>
+      <Text style={styles.field}>Kruunu</Text>
+      <Text style={styles.field}>{krona}</Text>
+      <Button title='Laske!' onPress={calculate}/>
     </View>
   );
 }
@@ -14,7 +29,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+
   },
+  field: {
+    marginBottom: 10,
+  }
 });
